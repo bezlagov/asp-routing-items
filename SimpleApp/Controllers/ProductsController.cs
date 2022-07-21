13a90034fce997eafd9e8ab09eae424cf67abbc5
Ivan Zhyhalkin <ivan.zhigalkin@gmail.com>
2022-07-21 16:45:39 +0300

    Add project files.

---
+++ SimpleApp/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductReader _reader;
+
+        // ВНИМАНИЕ.
+        // Каждый запрос обрабатывает новый экземпляр контроллера.
+        // Конструктор будет вызываться перед вызовом метода List и метода Details
+        // После обработки запроса, экземпляр контроллера будет удален из памяти
+        public ProductsController()
+        {
+            _reader = new ProductReader();
+        }
+
+        // Products/List
+        public IActionResult List(string category)
+        {
+            List<Product> products = _reader.ReadFromFile();
+
+            if (category != null)
+            { 
+                return View(products.Where(x=>x.Category.ToLower() == category.ToLower()).ToList());
+            }
+            // Возврат представления List и передача представлению модели в виде коллекции products
+            // Получить доступ к коллекции в представлении можно будет через свойство представления Model
+            return View(products);
+        }
+
+        // Products/Details/1
+        public IActionResult Details(int id)
+        {
+            List<Product> products = _reader.ReadFromFile();
+            Product product = products.Where(x => x.Id == id).FirstOrDefault();
+
+            if (product != null)
+            {
+                // Возврат представления с именем Details и передача представлению экземпляра product
+                // В представление доступ к экземпляру можно получить через свойство представления Model
+                return View(product);
+            }
+            else
+            {
+                // Возврат ошибки 404
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ SimpleApp/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SimpleApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // AddMvc - добавляет сервисы, необходимые для работы MVC, включая Razor-страницы
+            // services.AddMvc();
+
+            // AddControllersWithViews - добавляет сервисы, необходимые для работы MVC, 
+            // не включая Razor-страницы - только контроллеры и представления.
+            // Для примеров данного курса этого достаточно.
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "Default",
+                    pattern: "{controller}/{action}/{category}"
+                );
+                endpoints.MapControllerRoute(
+                        name: "Default",
+                        pattern: "{controller}/{action}"
+                    );
+            });
+
+            // {id?} - данный фрагмент шаблона описывает не обязательный сегмент в адресе запроса.
+            // При этом в контроллерах по имени id можно будет получить информацию, которая пришла в запросе
+            // Products/Details/10
+            // {controller} = Products
+            // {action} = Details
+            // {id} = 10
+        }
+    }
+}
